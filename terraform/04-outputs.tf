b746079a1803ed5c2ae911e47ca5b3111a883ff8
saranshan313 <sar.mca05@gmail.com>
2024-04-23 22:27:09 +1000

    revert output

--- terraform/04-outputs.tf
+++ terraform/04-outputs.tf
@@ -1,5 +1,5 @@
 output "aws_auth_data" {
   description = "Data of the AWS Auth Config map"
-  value       = try(yamldecode(data.kubernetes_config_map.deafult_aws_auth.data), null)
+  value       = try(data.kubernetes_config_map.deafult_aws_auth.data, null)
 }
 
