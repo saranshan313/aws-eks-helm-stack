b3bd5fea3538f6aa73cc5b1f69bc59c2b30b652f
saranshan313 <sar.mca05@gmail.com>
2024-04-23 20:08:41 +1000

    ran format

--- terraform/01-main.tf
+++ terraform/01-main.tf
@@ -6,13 +6,13 @@ locals {
 }
 
 provider "kubernetes" {
-  host = data.terraform_remote_state.eks.outputs.eks_endpoint
+  host                   = data.terraform_remote_state.eks.outputs.eks_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_ca)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.eks_cluster_name]
-    command = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.eks_cluster_name]
+    command     = "aws"
   }
 }
 
