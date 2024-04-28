582adab268a57759ca08a1609d28cfad6538fe90
saranshan313 <sar.mca05@gmail.com>
2024-04-28 14:08:28 +1000

    deploy helm charts

--- terraform/01-main.tf
+++ terraform/01-main.tf
@@ -63,7 +63,7 @@ data "terraform_remote_state" "eks" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "tf-remote-state-234-343-555-666"
+    bucket = "tf-remote-state-234-343-555-666-777"
     key    = "env:/infra-${local.settings.env}-${local.settings.region}/infra-${local.settings.env}-${local.settings.region}.tfstate"
     region = local.regions[local.settings.region]
   }
