4f240ab0350920ee19cc444edc8e5015dd944cde
saranshan313 <sar.mca05@gmail.com>
2024-04-30 10:02:52 +1000

    update the yaml error

--- terraform/05-eks-helm.tf
+++ terraform/05-eks-helm.tf
@@ -2,7 +2,7 @@
 locals {
   aws_auth_configmap_data = yamlencode({
     "data" : {
-      mapRoles : concat(yamldecode(data.kubernetes_config_map.deafult_aws_auth.data.mapRoles), local.settings.eks_cluster.aws_auth_config.cluster_admin)
+      mapRoles : yamlencode(concat(yamldecode(data.kubernetes_config_map.deafult_aws_auth.data.mapRoles), local.settings.eks_cluster.aws_auth_config.cluster_admin))
       #mapUsers : yamlencode(local.settings.eks_cluster.aws_auth_config.cluster_admin)
       #      mapAccounts = yamlencode(local.map_accounts)
     }
@@ -22,55 +22,55 @@ ${local.aws_auth_configmap_data}
 YAML
 }
 
-#Install ALB Ingress Controller
-resource "helm_release" "alb_controller" {
+# #Install ALB Ingress Controller
+# resource "helm_release" "alb_controller" {
 
-  name       = local.settings.alb_ingress_controller.chart_name
-  chart      = local.settings.alb_ingress_controller.chart_release_name
-  repository = local.settings.alb_ingress_controller.chart_repo_url
-  #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  #repository_password = data.aws_ecrpublic_authorization_token.token.password
-  version   = local.settings.alb_ingress_controller.chart_version
-  namespace = local.settings.alb_ingress_controller.namespace
+#   name       = local.settings.alb_ingress_controller.chart_name
+#   chart      = local.settings.alb_ingress_controller.chart_release_name
+#   repository = local.settings.alb_ingress_controller.chart_repo_url
+#   #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+#   #repository_password = data.aws_ecrpublic_authorization_token.token.password
+#   version   = local.settings.alb_ingress_controller.chart_version
+#   namespace = local.settings.alb_ingress_controller.namespace
 
-  create_namespace = local.settings.alb_ingress_controller.create_namespace
+#   create_namespace = local.settings.alb_ingress_controller.create_namespace
 
-  set {
-    name  = "clusterName"
-    value = data.terraform_remote_state.eks.outputs.eks_cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = data.terraform_remote_state.eks.outputs.eks_cluster_name
+#   }
 
-  set {
-    name  = "awsRegion"
-    value = local.regions[local.settings.region]
-  }
+#   set {
+#     name  = "awsRegion"
+#     value = local.regions[local.settings.region]
+#   }
 
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
+#   set {
+#     name  = "rbac.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = local.settings.alb_ingress_controller.service_account
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = local.settings.alb_ingress_controller.service_account
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.alb_controller_role.arn
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.alb_controller_role.arn
+#   }
 
-  set {
-    name  = "enableServiceMutatorWebhook"
-    value = "false"
-  }
+#   set {
+#     name  = "enableServiceMutatorWebhook"
+#     value = "false"
+#   }
 
-}
+# }
 
 #Install Karpenter Controller
 resource "aws_ec2_tag" "applications_subnets" {
