893c26119f06c3f2ba37e6eafe1e2a7b9b39a326
saranshan313 <sar.mca05@gmail.com>
2024-05-01 11:34:43 +1000

    update aws auth

--- terraform/01-main.tf
+++ terraform/01-main.tf
@@ -57,20 +57,20 @@ provider "http" {}
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
-    bucket = "tf-remote-state-234-343-555"
+    bucket = "tf-remote-state-234-343-555-666"
     key    = "env:/eks-${local.settings.env}-${local.settings.region}/eks-${local.settings.env}-${local.settings.region}.tfstate"
     region = local.regions[local.settings.region]
   }
 }
 
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "tf-remote-state-234-343-555"
-    key    = "env:/infra-${local.settings.env}-${local.settings.region}/infra-${local.settings.env}-${local.settings.region}.tfstate"
-    region = local.regions[local.settings.region]
-  }
-}
+# data "terraform_remote_state" "vpc" {
+#   backend = "s3"
+#   config = {
+#     bucket = "tf-remote-state-234-343-555-666"
+#     key    = "env:/infra-${local.settings.env}-${local.settings.region}/infra-${local.settings.env}-${local.settings.region}.tfstate"
+#     region = local.regions[local.settings.region]
+#   }
+# }
 
 data "aws_caller_identity" "current" {}
 
@@ -86,26 +86,26 @@ data "kubernetes_config_map" "deafult_aws_auth" {
   }
 }
 
-data "http" "karpenter_nodepool_crd" {
-  url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.36.0/pkg/apis/crds/karpenter.sh_nodepools.yaml"
-}
+# data "http" "karpenter_nodepool_crd" {
+#   url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.36.0/pkg/apis/crds/karpenter.sh_nodepools.yaml"
+# }
 
-data "kubectl_file_documents" "karpenter_nodepool_crd" {
-  content = data.http.karpenter_nodepool_crd.response_body
-}
+# data "kubectl_file_documents" "karpenter_nodepool_crd" {
+#   content = data.http.karpenter_nodepool_crd.response_body
+# }
 
-data "http" "karpenter_nodeclass_crd" {
-  url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.36.0/pkg/apis/crds/karpenter.k8s.aws_ec2nodeclasses.yaml"
-}
+# data "http" "karpenter_nodeclass_crd" {
+#   url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.36.0/pkg/apis/crds/karpenter.k8s.aws_ec2nodeclasses.yaml"
+# }
 
-data "kubectl_file_documents" "karpenter_nodeclass_crd" {
-  content = data.http.karpenter_nodeclass_crd.response_body
-}
+# data "kubectl_file_documents" "karpenter_nodeclass_crd" {
+#   content = data.http.karpenter_nodeclass_crd.response_body
+# }
 
-data "http" "karpenter_nodeclaim_crd" {
-  url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.36.0/pkg/apis/crds/karpenter.sh_nodeclaims.yaml"
-}
+# data "http" "karpenter_nodeclaim_crd" {
+#   url = "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v0.36.0/pkg/apis/crds/karpenter.sh_nodeclaims.yaml"
+# }
 
-data "kubectl_file_documents" "karpenter_nodeclaim_crd" {
-  content = data.http.karpenter_nodeclaim_crd.response_body
-}
+# data "kubectl_file_documents" "karpenter_nodeclaim_crd" {
+#   content = data.http.karpenter_nodeclaim_crd.response_body
+# }
--- terraform/05-eks-helm.tf
+++ terraform/05-eks-helm.tf
@@ -2,8 +2,9 @@
 locals {
   aws_auth_configmap_data = yamlencode({
     "data" : {
-      mapRoles : yamlencode(concat(yamldecode(data.kubernetes_config_map.deafult_aws_auth.data.mapRoles), local.settings.eks_cluster.aws_auth_config.cluster_admin))
-      #mapUsers : yamlencode(local.settings.eks_cluster.aws_auth_config.cluster_admin)
+      #mapRoles : yamlencode(concat(yamldecode(data.kubernetes_config_map.deafult_aws_auth.data.mapRoles), local.settings.eks_cluster.aws_auth_config.cluster_admin))
+      mapRoles : yamlencode(data.kubernetes_config_map.deafult_aws_auth.data.mapRoles)
+      mapUsers : yamlencode(local.settings.eks_cluster.aws_auth_config.cluster_admin)
       #      mapAccounts = yamlencode(local.map_accounts)
     }
   })
@@ -22,209 +23,209 @@ ${local.aws_auth_configmap_data}
 YAML
 }
 
-#Install ALB Ingress Controller
-resource "helm_release" "alb_controller" {
+# #Install ALB Ingress Controller
+# resource "helm_release" "alb_controller" {
 
-  name       = local.settings.alb_ingress_controller.chart_name
-  chart      = local.settings.alb_ingress_controller.chart_release_name
-  repository = local.settings.alb_ingress_controller.chart_repo_url
-  #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  #repository_password = data.aws_ecrpublic_authorization_token.token.password
-  version   = local.settings.alb_ingress_controller.chart_version
-  namespace = local.settings.alb_ingress_controller.namespace
+#   name       = local.settings.alb_ingress_controller.chart_name
+#   chart      = local.settings.alb_ingress_controller.chart_release_name
+#   repository = local.settings.alb_ingress_controller.chart_repo_url
+#   #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+#   #repository_password = data.aws_ecrpublic_authorization_token.token.password
+#   version   = local.settings.alb_ingress_controller.chart_version
+#   namespace = local.settings.alb_ingress_controller.namespace
 
-  create_namespace = local.settings.alb_ingress_controller.create_namespace
+#   create_namespace = local.settings.alb_ingress_controller.create_namespace
 
-  set {
-    name  = "clusterName"
-    value = data.terraform_remote_state.eks.outputs.eks_cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = data.terraform_remote_state.eks.outputs.eks_cluster_name
+#   }
 
-  set {
-    name  = "awsRegion"
-    value = local.regions[local.settings.region]
-  }
+#   set {
+#     name  = "awsRegion"
+#     value = local.regions[local.settings.region]
+#   }
 
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
+#   set {
+#     name  = "rbac.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = local.settings.alb_ingress_controller.service_account
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = local.settings.alb_ingress_controller.service_account
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.alb_controller_role.arn
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.alb_controller_role.arn
+#   }
 
-  set {
-    name  = "enableServiceMutatorWebhook"
-    value = "false"
-  }
+#   set {
+#     name  = "enableServiceMutatorWebhook"
+#     value = "false"
+#   }
 
-  set {
-    name  = "image.repository"
-    value = "602401143452.dkr.ecr.${local.regions[local.settings.region]}.amazonaws.com/amazon/aws-load-balancer-controller"
-  }
+#   set {
+#     name  = "image.repository"
+#     value = "602401143452.dkr.ecr.${local.regions[local.settings.region]}.amazonaws.com/amazon/aws-load-balancer-controller"
+#   }
 
-  set {
-    name  = "image.tag"
-    value = "v2.7.2"
-  }
+#   set {
+#     name  = "image.tag"
+#     value = "v2.7.2"
+#   }
 
-}
+# }
 
-#Install Karpenter Controller
-resource "aws_ec2_tag" "applications_subnets" {
-  for_each    = data.terraform_remote_state.vpc.outputs.network_application_subnets
-  resource_id = each.value
-  key         = "karpenter.sh/discovery"
-  value       = data.terraform_remote_state.eks.outputs.eks_cluster_name
-}
+# #Install Karpenter Controller
+# resource "aws_ec2_tag" "applications_subnets" {
+#   for_each    = data.terraform_remote_state.vpc.outputs.network_application_subnets
+#   resource_id = each.value
+#   key         = "karpenter.sh/discovery"
+#   value       = data.terraform_remote_state.eks.outputs.eks_cluster_name
+# }
 
-resource "aws_ec2_tag" "nodegrp_sgs" {
-  for_each    = toset(data.terraform_remote_state.eks.outputs.eks_nodegrp_sgs)
-  resource_id = each.value
-  key         = "karpenter.sh/discovery"
-  value       = data.terraform_remote_state.eks.outputs.eks_cluster_name
-}
+# resource "aws_ec2_tag" "nodegrp_sgs" {
+#   for_each    = toset(data.terraform_remote_state.eks.outputs.eks_nodegrp_sgs)
+#   resource_id = each.value
+#   key         = "karpenter.sh/discovery"
+#   value       = data.terraform_remote_state.eks.outputs.eks_cluster_name
+# }
 
-resource "helm_release" "karpenter_controller" {
-  name       = local.settings.karpenter_controller.chart_name
-  chart      = local.settings.karpenter_controller.chart_release_name
-  repository = local.settings.karpenter_controller.chart_repo_url
-  #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  #repository_password = data.aws_ecrpublic_authorization_token.token.password
-  version          = local.settings.karpenter_controller.chart_version
-  namespace        = local.settings.karpenter_controller.namespace
-  wait             = false
-  create_namespace = local.settings.karpenter_controller.create_namespace
+# resource "helm_release" "karpenter_controller" {
+#   name       = local.settings.karpenter_controller.chart_name
+#   chart      = local.settings.karpenter_controller.chart_release_name
+#   repository = local.settings.karpenter_controller.chart_repo_url
+#   #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+#   #repository_password = data.aws_ecrpublic_authorization_token.token.password
+#   version          = local.settings.karpenter_controller.chart_version
+#   namespace        = local.settings.karpenter_controller.namespace
+#   wait             = false
+#   create_namespace = local.settings.karpenter_controller.create_namespace
 
-  set {
-    name  = "clusterName"
-    value = data.terraform_remote_state.eks.outputs.eks_cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = data.terraform_remote_state.eks.outputs.eks_cluster_name
+#   }
 
-  set {
-    name  = "clusterEnpoint"
-    value = data.terraform_remote_state.eks.outputs.eks_endpoint
-  }
+#   set {
+#     name  = "clusterEnpoint"
+#     value = data.terraform_remote_state.eks.outputs.eks_endpoint
+#   }
 
-  set {
-    name  = "awsRegion"
-    value = local.regions[local.settings.region]
-  }
+#   set {
+#     name  = "awsRegion"
+#     value = local.regions[local.settings.region]
+#   }
 
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
+#   set {
+#     name  = "rbac.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = local.settings.alb_ingress_controller.service_account
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = local.settings.alb_ingress_controller.service_account
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.karpenter_controller_role.arn
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.karpenter_controller_role.arn
+#   }
 
-  set {
-    name  = "defaultInstanceProfile"
-    value = data.terraform_remote_state.eks.outputs.eks_node_instance_profile
-  }
+#   set {
+#     name  = "defaultInstanceProfile"
+#     value = data.terraform_remote_state.eks.outputs.eks_node_instance_profile
+#   }
 
-  set {
-    name  = "image.repository"
-    value = "public.ecr.aws/karpenter/controller"
-  }
+#   set {
+#     name  = "image.repository"
+#     value = "public.ecr.aws/karpenter/controller"
+#   }
 
-  set {
-    name  = "image.tag"
-    value = "0.36.1"
-  }
-}
+#   set {
+#     name  = "image.tag"
+#     value = "0.36.1"
+#   }
+# }
 
-resource "kubectl_manifest" "karpenter_crd_nodepool" {
-  for_each  = data.kubectl_file_documents.karpenter_nodepool_crd.manifests
-  yaml_body = each.value
-}
+# resource "kubectl_manifest" "karpenter_crd_nodepool" {
+#   for_each  = data.kubectl_file_documents.karpenter_nodepool_crd.manifests
+#   yaml_body = each.value
+# }
 
-resource "kubectl_manifest" "karpenter_crd_nodeclass" {
-  for_each  = data.kubectl_file_documents.karpenter_nodeclass_crd.manifests
-  yaml_body = each.value
-}
+# resource "kubectl_manifest" "karpenter_crd_nodeclass" {
+#   for_each  = data.kubectl_file_documents.karpenter_nodeclass_crd.manifests
+#   yaml_body = each.value
+# }
 
-resource "kubectl_manifest" "karpenter_crd_nodeclaim" {
-  for_each  = data.kubectl_file_documents.karpenter_nodeclaim_crd.manifests
-  yaml_body = each.value
-}
+# resource "kubectl_manifest" "karpenter_crd_nodeclaim" {
+#   for_each  = data.kubectl_file_documents.karpenter_nodeclaim_crd.manifests
+#   yaml_body = each.value
+# }
 
-resource "kubectl_manifest" "karpenter_nodepool_nodeclass" {
-  yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1beta1
-    kind: NodePool
-    metadata:
-      name: default
-    spec:
-      template:
-        spec:
-          requirements:
-            - key: kubernetes.io/arch
-              operator: In
-              values: ["amd64"]
-            - key: kubernetes.io/os
-              operator: In
-              values: ["linux"]
-            - key: karpenter.sh/capacity-type
-              operator: In
-              values: ["spot"]
-            - key: karpenter.k8s.aws/instance-category
-              operator: In
-              values: ["c", "m", "r"]
-            - key: karpenter.k8s.aws/instance-generation
-              operator: Gt
-              values: ["2"]
-          nodeClassRef:
-            apiVersion: karpenter.k8s.aws/v1beta1
-            kind: EC2NodeClass
-            name: default
-      limits:
-        cpu: 1000
-      disruption:
-        consolidationPolicy: WhenUnderutilized
-        expireAfter: 720h # 30 * 24h = 720h
-    ---
-    apiVersion: karpenter.k8s.aws/v1beta1
-    kind: EC2NodeClass
-    metadata:
-      name: default
-    spec:
-      amiFamily: AL2 # Amazon Linux 2
-      role: "KarpenterNodeRole-${data.terraform_remote_state.eks.outputs.eks_cluster_name}" # replace with your cluster name
-      subnetSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: "${data.terraform_remote_state.eks.outputs.eks_cluster_name}" # replace with your cluster name
-      securityGroupSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: "${data.terraform_remote_state.eks.outputs.eks_cluster_name}" # replace with your cluster name
-  YAML
+# resource "kubectl_manifest" "karpenter_nodepool_nodeclass" {
+#   yaml_body = <<-YAML
+#     apiVersion: karpenter.sh/v1beta1
+#     kind: NodePool
+#     metadata:
+#       name: default
+#     spec:
+#       template:
+#         spec:
+#           requirements:
+#             - key: kubernetes.io/arch
+#               operator: In
+#               values: ["amd64"]
+#             - key: kubernetes.io/os
+#               operator: In
+#               values: ["linux"]
+#             - key: karpenter.sh/capacity-type
+#               operator: In
+#               values: ["spot"]
+#             - key: karpenter.k8s.aws/instance-category
+#               operator: In
+#               values: ["c", "m", "r"]
+#             - key: karpenter.k8s.aws/instance-generation
+#               operator: Gt
+#               values: ["2"]
+#           nodeClassRef:
+#             apiVersion: karpenter.k8s.aws/v1beta1
+#             kind: EC2NodeClass
+#             name: default
+#       limits:
+#         cpu: 1000
+#       disruption:
+#         consolidationPolicy: WhenUnderutilized
+#         expireAfter: 720h # 30 * 24h = 720h
+#     ---
+#     apiVersion: karpenter.k8s.aws/v1beta1
+#     kind: EC2NodeClass
+#     metadata:
+#       name: default
+#     spec:
+#       amiFamily: AL2 # Amazon Linux 2
+#       role: "KarpenterNodeRole-${data.terraform_remote_state.eks.outputs.eks_cluster_name}" # replace with your cluster name
+#       subnetSelectorTerms:
+#         - tags:
+#             karpenter.sh/discovery: "${data.terraform_remote_state.eks.outputs.eks_cluster_name}" # replace with your cluster name
+#       securityGroupSelectorTerms:
+#         - tags:
+#             karpenter.sh/discovery: "${data.terraform_remote_state.eks.outputs.eks_cluster_name}" # replace with your cluster name
+#   YAML
 
-  depends_on = [
-    helm_release.karpenter_controller
-  ]
-}
+#   depends_on = [
+#     helm_release.karpenter_controller
+#   ]
+# }
