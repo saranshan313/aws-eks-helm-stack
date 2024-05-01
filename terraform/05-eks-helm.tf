c983321cf5c7953abfc05630d4572bca015a31e0
saranshan313 <sar.mca05@gmail.com>
2024-05-01 11:36:30 +1000

    update the auth data

--- terraform/05-eks-helm.tf
+++ terraform/05-eks-helm.tf
@@ -3,7 +3,7 @@ locals {
   aws_auth_configmap_data = yamlencode({
     "data" : {
       #mapRoles : yamlencode(concat(yamldecode(data.kubernetes_config_map.deafult_aws_auth.data.mapRoles), local.settings.eks_cluster.aws_auth_config.cluster_admin))
-      mapRoles : yamlencode(data.kubernetes_config_map.deafult_aws_auth.data.mapRoles)
+      mapRoles : data.kubernetes_config_map.deafult_aws_auth.data.mapRoles
       mapUsers : yamlencode(local.settings.eks_cluster.aws_auth_config.cluster_admin)
       #      mapAccounts = yamlencode(local.map_accounts)
     }
