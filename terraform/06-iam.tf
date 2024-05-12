ae3089fbb172dbf73f1915ee04818931df184314
saranshan313 <sar.mca05@gmail.com>
2024-05-12 15:08:44 +1000

    add role for eks app

--- terraform/06-iam.tf
+++ terraform/06-iam.tf
@@ -115,3 +115,70 @@ resource "aws_iam_role_policy_attachment" "karpenter_controller_permissions" {
   role       = aws_iam_role.karpenter_controller_role.name
   policy_arn = aws_iam_policy.karpenter_controller_permission_policy.arn
 }
+
+#IAM Role for EKS Apps
+data "aws_iam_policy_document" "eks_app_assume_policy" {
+
+  statement {
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type = "Federated"
+      identifiers = [
+        data.terraform_remote_state.eks.outputs.eks_oidc_arn
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.terraform_remote_state.eks.outputs.eks_oidc_issuer, "https://", "")}:sub"
+
+      values = [
+        "system:serviceaccount:${local.settings.eks_app_role.namespace}:${local.settings.eks_app_role.service_account}",
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.terraform_remote_state.eks.outputs.eks_oidc_issuer, "https://", "")}:aud"
+
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "eks_app_role" {
+  name               = "role-${local.settings.env}-${local.settings.region}-eks-app-01"
+  description        = "Role for the EKS Registration App"
+  assume_role_policy = data.aws_iam_policy_document.eks_app_assume_policy.json
+}
+
+resource "aws_iam_policy" "eks_app_permission_policy" {
+  name        = "policy-${local.settings.env}-${local.settings.region}-eks-app-01"
+  path        = "/"
+  description = "Permission policy for the KS Registration App"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Effect   = "Allow"
+        Resource = data.terraform_remote_state.eks.outputs.rds_secret_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_app_permissions" {
+  role       = aws_iam_role.eks_app_role.name
+  policy_arn = aws_iam_policy.eks_app_permission_policy.arn
+}
