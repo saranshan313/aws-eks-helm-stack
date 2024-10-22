53ce5c5da0f1e412cda93137987a1f7e97cf2777
saranshan313 <sar.mca05@gmail.com>
2024-10-22 15:11:25 +1100

    commented aws lb controller

--- terraform/05-eks-helm.tf
+++ terraform/05-eks-helm.tf
@@ -69,80 +69,80 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
 # }
 
 #Install ALB Ingress Controller
-resource "aws_ec2_tag" "alb_controller_public_subnets_tags" {
-  for_each    = data.terraform_remote_state.vpc.outputs.network_public_subnets
-  resource_id = each.value
-  key         = "kubernetes.io/role/elb"
-  value       = "1"
-}
+# resource "aws_ec2_tag" "alb_controller_public_subnets_tags" {
+#   for_each    = data.terraform_remote_state.vpc.outputs.network_public_subnets
+#   resource_id = each.value
+#   key         = "kubernetes.io/role/elb"
+#   value       = "1"
+# }
 
-resource "aws_ec2_tag" "alb_controller_public_private_subnets_tags" {
-  for_each = merge(
-    data.terraform_remote_state.vpc.outputs.network_public_subnets,
-    data.terraform_remote_state.vpc.outputs.network_application_subnets
-  )
-  resource_id = each.value
-  key         = "kubernetes.io/cluster/${data.terraform_remote_state.eks.outputs.eks_cluster_name}"
-  value       = "owned"
-}
+# resource "aws_ec2_tag" "alb_controller_public_private_subnets_tags" {
+#   for_each = merge(
+#     data.terraform_remote_state.vpc.outputs.network_public_subnets,
+#     data.terraform_remote_state.vpc.outputs.network_application_subnets
+#   )
+#   resource_id = each.value
+#   key         = "kubernetes.io/cluster/${data.terraform_remote_state.eks.outputs.eks_cluster_name}"
+#   value       = "owned"
+# }
 
-resource "helm_release" "alb_controller" {
-  name       = local.settings.alb_ingress_controller.chart_name
-  chart      = local.settings.alb_ingress_controller.chart_release_name
-  repository = local.settings.alb_ingress_controller.chart_repo_url
-  #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  #repository_password = data.aws_ecrpublic_authorization_token.token.password
-  version   = local.settings.alb_ingress_controller.chart_version
-  namespace = local.settings.alb_ingress_controller.namespace
+# resource "helm_release" "alb_controller" {
+#   name       = local.settings.alb_ingress_controller.chart_name
+#   chart      = local.settings.alb_ingress_controller.chart_release_name
+#   repository = local.settings.alb_ingress_controller.chart_repo_url
+#   #repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+#   #repository_password = data.aws_ecrpublic_authorization_token.token.password
+#   version   = local.settings.alb_ingress_controller.chart_version
+#   namespace = local.settings.alb_ingress_controller.namespace
 
-  create_namespace = local.settings.alb_ingress_controller.create_namespace
+#   create_namespace = local.settings.alb_ingress_controller.create_namespace
 
-  set {
-    name  = "clusterName"
-    value = data.terraform_remote_state.eks.outputs.eks_cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = data.terraform_remote_state.eks.outputs.eks_cluster_name
+#   }
 
-  set {
-    name  = "awsRegion"
-    value = local.regions[local.settings.region]
-  }
+#   set {
+#     name  = "awsRegion"
+#     value = local.regions[local.settings.region]
+#   }
 
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
+#   set {
+#     name  = "rbac.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = local.settings.alb_ingress_controller.service_account
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = local.settings.alb_ingress_controller.service_account
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.alb_controller_role.arn
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.alb_controller_role.arn
+#   }
 
-  set {
-    name  = "enableServiceMutatorWebhook"
-    value = "false"
-  }
+#   set {
+#     name  = "enableServiceMutatorWebhook"
+#     value = "false"
+#   }
 
-  # set {
-  #   name  = "image.repository"
-  #   value = "602401143452.dkr.ecr.${local.regions[local.settings.region]}.amazonaws.com/amazon/aws-load-balancer-controller"
-  # }
+#   # set {
+#   #   name  = "image.repository"
+#   #   value = "602401143452.dkr.ecr.${local.regions[local.settings.region]}.amazonaws.com/amazon/aws-load-balancer-controller"
+#   # }
 
-  # set {
-  #   name  = "image.tag"
-  #   value = "v2.7.2"
-  # }
+#   # set {
+#   #   name  = "image.tag"
+#   #   value = "v2.7.2"
+#   # }
 
-}
+# }
 
 #Install Karpenter Controller
 resource "aws_ec2_tag" "karpenter_subnets_tags" {
